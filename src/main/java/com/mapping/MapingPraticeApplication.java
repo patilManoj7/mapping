4f573753803c25ba474cd39d29d15463321b7f58
patilManoj7 <manojpatil72002@gmail.com>
2024-10-05 14:30:10 +0530

    inital commit

---
+++ src/main/java/com/mapping/MapingPraticeApplication.java
@@ -0,0 +1,13 @@
+package com.mapping;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MapingPraticeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MapingPraticeApplication.class, args);
+	}
+
+}
